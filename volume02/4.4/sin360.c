d87b7d27c0d30f736e539ab39dd8c0c561fb52d3
Max Kulish <kma.memo@gmail.com>
2022-02-03 09:44:18 +0200

    sin360 file

---
+++ volume02/4.4/sin360.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    FILE *f;
+    int grad;
+
+    f = fopen("sincons.txt", "w");
+    if (!f) {
+        perror("sincos.txt");
+        return 1;
+    }
+
+    for (grad = 0; grad < 360; grad++) {
+        double rads, s, c;
+        rads = (double)grad * M_PI / 180.0;
+        s = sin(rads);
+        c = cos(rads);
+
+        fprintf(f, "%03d % 7.5f % 7.5f\n", grad, s, c);
+    }
+    fclose(f);
+    return 0;
+}
